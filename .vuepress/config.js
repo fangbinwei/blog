ba78f1b05fd84a3d1752b17db327374de2746f72
fangbinwei <fangbinwei@yeah.net>
2019-10-07 22:10:13 +0800

    feat: add mathjax,medium-zoom

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
   base: process.env.GITHUB_PAGE ? '/blog/' : '/',
   title: `Sleepwalker's Blog`,
   description: `It's a technology blog focuses on front-end development, but not only that.`,
+  plugins: [
+    ['@vuepress/medium-zoom', { selector: 'img' }],
+    ['vuepress-plugin-mathjax']
+  ],
   markdown: {
     lineNumbers: true
   },

62ef1c05bcfa55cc85c3e2c399820357e2bbdb32
fangbinwei <fangbinwei@yeah.net>
2019-11-10 13:46:34 +0800

    fix(plugin): use git log --follow

--- .vuepress/plugins/autoNavSidebar/util/git.js
+++ .vuepress/plugins/autoNavSidebar/util/git.js
@@ -1,15 +1,23 @@
 const spawn = require('cross-spawn')
 const path = require('path')
 const os = require('os')
+const debug = require('debug')('myPlugin-autoNavSidebar-git')
 
 function getGitAddedTimeStamp(filePath) {
   let earliest
+  debug('filePath', filePath)
   try {
     // git log 文件夹的时间戳有多个
     const addedTimeStampHistory = spawn
       .sync(
         'git',
-        ['log', '--diff-filter=A', '--format=%at', path.basename(filePath)],
+        [
+          'log',
+          '--follow', // continue listening the history of renamed file
+          '--diff-filter=A',
+          '--format=%at',
+          path.basename(filePath)
+        ],
         {
           cwd: path.dirname(filePath)
         }
@@ -21,7 +29,9 @@ function getGitAddedTimeStamp(filePath) {
       .split(os.EOL)
       .slice(-1)
       .toString()
+    debug('earliest', earliest)
   } catch (e) {
+    debug('catch', e)
     return Number.MAX_SAFE_INTEGER
   }
   return earliest === '' ? Number.MAX_SAFE_INTEGER : Number(earliest)
